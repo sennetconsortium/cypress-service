25688b2f9f4bfe0da215bfb231157f3b1e94016a
Lisa-Ann B <lisabruney@pitt.edu>
2024-12-20 11:20:34 -0500

    Remove unnecessary generated css classes

--- src/pages/index.js
+++ src/pages/index.js
@@ -129,10 +129,10 @@ export default function Home() {
     <>
     <AppNav handleConnect={connect} isConnected={isConnected} />
       <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
       
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="sm:items-start">
   
       {loading && <AppSpinner />}
        
