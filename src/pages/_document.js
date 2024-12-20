e3c3e27c6b7a14e067944be1d081175b93e343eb
Lisa-Ann B <lisabruney@pitt.edu>
2024-12-20 10:38:49 -0500

    Update name

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>SenNet Cypress Service</title>
+        </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
